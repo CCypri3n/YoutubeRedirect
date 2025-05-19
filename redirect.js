86e297d1353be14f3ead6f2aa589f6e43db41e97
Cyprien <108684496+CCypri3n@users.noreply.github.com>
2025-05-19 21:57:04 +0200

    Repaired the js

--- redirect.js
+++ redirect.js
@@ -5,7 +5,6 @@
     const theme = 'dark';
     const hl = 'fr';
     const cc_lang = 'fr';
-    const 
     if (videoId) {
       // Construct your custom URL
       const targetUrl = `https://www.youtube-nocookie.com/embed/${videoId}?wmode=transparent&iv_load_policy=3&autoplay=1&html5=1&showinfo=0&rel=0&modestbranding=1&playsinline=0&theme=${theme}&hl=${hl}&cc_lang_pref=${cc_lang}`;
