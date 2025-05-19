9761dd4871e3869888ce73437986266059980a8a
Cyprien <108684496+CCypri3n@users.noreply.github.com>
2025-05-19 21:54:36 +0200

    Added more options the no-cookie version in the js, removed useless lines from the manifest.

--- redirect.js
+++ redirect.js
@@ -2,9 +2,13 @@
     // Extract the video ID from the URL
     const params = new URLSearchParams(window.location.search);
     const videoId = params.get('v');
+    const theme = 'dark';
+    const hl = 'fr';
+    const cc_lang = 'fr';
+    const 
     if (videoId) {
       // Construct your custom URL
-      const targetUrl = `https://www.youtube-nocookie.com/embed/${videoId}?wmode=transparent&iv_load_policy=3&autoplay=1&html5=1&showinfo=0&rel=0&modestbranding=1&playsinline=0&theme=light`;
+      const targetUrl = `https://www.youtube-nocookie.com/embed/${videoId}?wmode=transparent&iv_load_policy=3&autoplay=1&html5=1&showinfo=0&rel=0&modestbranding=1&playsinline=0&theme=${theme}&hl=${hl}&cc_lang_pref=${cc_lang}`;
       // Redirect to the new site
       window.location.replace(targetUrl);
     }
