98cd795479b97a5afbd3e293c3f925cbaef5a175
Cyprien <Cython.Chev@gmail.com>
2025-05-20 11:47:31 +0200

    Added notifications (if the user wants them)

--- options.js
+++ options.js
@@ -6,14 +6,16 @@ function saveOptions() {
   const cc_lang = document.getElementById('cc_lang').value;
   const cc_load_policy = Number(document.getElementById('cc_load_policy').value); // convert to number
   const theme = document.getElementById('theme').value;
+  const notifications_allowed = document.getElementById('notifications_allowed').value;
 
-  browser.storage.local.set({ hl, cc_lang, cc_load_policy, theme }).then(() => {
+  browser.storage.local.set({ hl, cc_lang, cc_load_policy, theme, notifications_allowed}).then(() => {
     const status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(() => {
       status.textContent = '';
     }, 2000);
   });
+  browser.runtime.sendMessage({ log: `Options updated to ${hl + cc_lang + cc_load_policy + theme}` });
 }
 
 function restoreOptions() {
@@ -21,11 +23,13 @@ function restoreOptions() {
     hl: 'en',
     cc_lang: 'en',
     cc_load_policy: 1,
-    theme: 'dark'
+    theme: 'dark',
+    notifications_allowed: 1
   }).then((result) => {
     document.getElementById('hl').value = result.hl;
     document.getElementById('cc_lang').value = result.cc_lang;
     document.getElementById('cc_load_policy').value = result.cc_load_policy;
     document.getElementById('theme').value = result.theme;
+    document.getElementById('notifications_allowed').value = result.notifications_allowed;
   });
 }
--- redirect.js
+++ redirect.js
@@ -14,6 +14,7 @@
 
   if (videoId && currentUrl.includes("youtube.com/watch")) {
     const targetUrl = `https://www.youtube-nocookie.com/embed/${videoId}?wmode=transparent&iv_load_policy=3&autoplay=1&html5=1&showinfo=0&rel=0&modestbranding=1&playsinline=0&theme=${options.theme}&hl=${options.hl}&cc_lang_pref=${options.cc_lang}&cc_load_policy=${options.cc_load_policy}`;
+    browser.runtime.sendMessage({ log: `targetUrl: ${targetUrl}` });
     window.location.replace(targetUrl);
 
   } else if (currentUrl.includes("youtube-nocookie.com/embed")) {
@@ -21,6 +22,7 @@
     const videoId = path.split('/embed/')[1];
     if (videoId) {
       const targetUrl = `https://youtube.com/watch?v=${videoId}`;
+      browser.runtime.sendMessage({ log: `targetUrl: ${targetUrl}` });
       window.location.replace(targetUrl);
     }
   }
